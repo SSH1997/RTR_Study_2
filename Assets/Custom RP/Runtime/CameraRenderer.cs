e748207b012b9463e49b2b611512505e2ed968c1
SSH1997 [akasiassh] <akasiassh@naver.com>
2024-02-16 23:50:58 +0900

    11주차 실습

--- Assets/Custom RP/Runtime/CameraRenderer.cs
+++ Assets/Custom RP/Runtime/CameraRenderer.cs
@@ -75,7 +75,9 @@ public partial class CameraRenderer
         var drawingSettings = new DrawingSettings(unlitShaderTagId, sortingSettings)
         {
             enableDynamicBatching = useDynamicBatching,
-            enableInstancing = useGPUInstancing
+            enableInstancing = useGPUInstancing,
+            perObjectData = PerObjectData.Lightmaps | PerObjectData.LightProbe|
+                            PerObjectData.LightProbeProxyVolume
         };
         drawingSettings.SetShaderPassName(1, litShaderTagId);
         var filteringSettings = new FilteringSettings(RenderQueueRange.opaque);
